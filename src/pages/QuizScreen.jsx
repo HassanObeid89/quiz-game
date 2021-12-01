d063535411fe472986782418adecfbaba86fb1c6
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-12-01 12:00:16 +0100

    set the limit of the questions to ten to finish the game.

--- src/pages/QuizScreen.jsx
+++ src/pages/QuizScreen.jsx
@@ -7,14 +7,17 @@ import questions from "../data/questions.json";
 export default function QuizScreen() {
   //Local state
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  
+  //Methods
+  function onAnswer() {
+    const nextQuestion = currentQuestion + 1;
+    nextQuestion < 10 ? setCurrentQuestion(nextQuestion):alert("finished");
+  }
 
   //properties
   const answer = questions[currentQuestion].answers.map(
     (answerOption, index) => (
-      <button
-        key={index}
-        onClick={() => setCurrentQuestion(currentQuestion + 1)}
-      >
+      <button key={index} onClick={onAnswer}>
         {answerOption.option}
       </button>
     )
