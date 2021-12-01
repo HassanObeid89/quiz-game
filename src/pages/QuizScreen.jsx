838c793cace49f5dd82755e972ef7ec90a9b103a
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-12-01 11:53:16 +0100

    Added onClick event to the options to show the next question.

--- src/pages/QuizScreen.jsx
+++ src/pages/QuizScreen.jsx
@@ -1,14 +1,29 @@
+//NPM Package
+import { useState } from "react";
+
+//Project files
 import questions from "../data/questions.json";
 
 export default function QuizScreen() {
-  const answer = questions[0].answers.map((answerOption, index) => (
-    <button key={index}>{answerOption.option}</button>
-  ));
-  
+  //Local state
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  //properties
+  const answer = questions[currentQuestion].answers.map(
+    (answerOption, index) => (
+      <button
+        key={index}
+        onClick={() => setCurrentQuestion(currentQuestion + 1)}
+      >
+        {answerOption.option}
+      </button>
+    )
+  );
+
   return (
     <div>
       <h1>Quiz page</h1>
-      {questions[0].text}
+      {questions[currentQuestion].text}
       <section>{answer}</section>
     </div>
   );
