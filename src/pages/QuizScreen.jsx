f15fda3360c820b3dcf42915614bdad810465080
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-12-01 11:39:39 +0100

    Displayed first question and alternative answers in the Quiz Screen.

--- src/pages/QuizScreen.jsx
+++ src/pages/QuizScreen.jsx
@@ -1,7 +1,15 @@
+import questions from "../data/questions.json";
+
 export default function QuizScreen() {
+  const answer = questions[0].answers.map((answerOption, index) => (
+    <button key={index}>{answerOption.option}</button>
+  ));
+  
   return (
     <div>
       <h1>Quiz page</h1>
+      {questions[0].text}
+      <section>{answer}</section>
     </div>
   );
 }
