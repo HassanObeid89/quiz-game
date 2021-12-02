9125e670e57902ebb90925e55ccabe6d808d986a
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-12-02 16:11:52 +0100

    Added instructions to the welcome page

--- src/pages/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen.jsx
@@ -7,7 +7,11 @@ export default function WelcomeScreen() {
 
   return (
     <div>
-      <h1>Welcome page</h1>
+      <h1>Are you ready to test your JavaScript skills?</h1>
+      <ul>
+        <li>You will have only 15 seconds per each question</li>
+        <li>You can use once those two options 'delete two wrong answers' & 'add more 10 seconds'</li>
+      </ul>
       <button onClick={() => setGameState("playing")}>Start Game</button>
     </div>
   );
