94e5a1146279e44c62c9d3e24c24f9b8ceecfdf7
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-12-02 16:19:52 +0100

    clean up

--- src/pages/WelcomeScreen.test.js
+++ src/pages/WelcomeScreen.test.js
@@ -1,15 +1,10 @@
 import { render } from "@testing-library/react";
 
-
 import WelcomeScreen from "./WelcomeScreen";
 
 describe("WelcomeScreen component", () => {
   it("Welcome screen should have Start Game button", () => {
-    const { getByTestId } = render(
-      
-        <WelcomeScreen />
-    
-    );
+    const { getByTestId } = render(<WelcomeScreen />);
     const buttonElement = getByTestId("startGame");
     expect(buttonElement).toBeTruthy();
   });
